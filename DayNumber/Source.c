ab2b25f31d968b8f9b59afd9ecc11653b65a7c31
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2021-10-07 14:55:25 +0100

    declared variables

---
+++ DayNumber/Source.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+	//Declare Variables
+	int day, month, year, dayNum;
+
+	//Ask user to input selected date
+	printf("Please enter Date you want to find number of\t");
+	scanf("%d %d %d", &day, &month, &year);
+
+	//First find if year is a leap year
+
+}
